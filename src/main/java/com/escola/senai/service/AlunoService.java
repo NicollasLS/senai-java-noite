02220528a4e16f15adb1c8416d4b377d21060dcc
Nicollas LS <nicollas.lima13@gmail.com>
2025-04-29 21:54:35 -0300

    feat: Serviços

--- src/main/java/com/escola/senai/service/AlunoService.java
+++ src/main/java/com/escola/senai/service/AlunoService.java
@@ -1,4 +1,30 @@
 package com.escola.senai.service;
 
+import com.escola.senai.Interface.AlunoRepository;
+import com.escola.senai.model.Aluno;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AlunoService {
+
+    private final AlunoRepository repository;
+
+    public AlunoService (AlunoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Aluno> ListarTodos(){
+        return repository.findAll();
+    }
+public Aluno salvar (Aluno aluno) {
+        return repository.save(aluno);
+}
+public void deletar (Long id){
+        repository.deleteAllById(id);
+}
+public Aluno buscarPorId (Long id){
+        return repository.findById(id).orElse(null);
+    }
 }
